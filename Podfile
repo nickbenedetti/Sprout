3aeb9cf2f2528cd9a1cdfd44e9fc5d20f0868a35
Levi Brown <levigroker@gmail.com>
2013-09-05 16:52:22 -0600

    Updating to SSZipArchive 0.2.5.
    Signed-off-by: Levi Brown <levigroker@gmail.com>

--- Podfile
+++ Podfile
@@ -1,3 +1,3 @@
 platform :ios, '5.0'
 pod 'CocoaLumberjack',	'~> 1.6'
-pod 'SSZipArchive',		'~> 0.2.4'
+pod 'SSZipArchive',		'~> 0.2.5'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+
+// SSZipArchive
+#define COCOAPODS_POD_AVAILABLE_SSZipArchive
+#define COCOAPODS_VERSION_MAJOR_SSZipArchive 0
+#define COCOAPODS_VERSION_MINOR_SSZipArchive 2
+#define COCOAPODS_VERSION_PATCH_SSZipArchive 5
+
